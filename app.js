a3ba9c7fe8163033ec8213434200fd3693cb7898
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-04-21 21:07:25 +0800

    first rebuild

--- app.js
+++ app.js
@@ -4,4 +4,24 @@ const app = Vue.createApp({
             energy: 3,
         };
     },
+    methods: {
+        addEnergy() {
+            if (this.energy < 10) this.energy++;
+        },
+        reduceEnergy() {
+            if (this.energy > 0) this.energy--;
+        },
+        endTurn() {
+            if (this.energy == 9) {
+                this.energy += 1;
+            } else if (this.energy < 10) {
+                this.energy += 2;
+            }
+        },
+        reset() {
+            this.energy = 3;
+        },
+    },
 });
+
+app.mount('#app');
